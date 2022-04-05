9ae4ca142d0b18e4489727d73bab0588c69b43ec
robynstuart <robynstuart@users.noreply.github.com>
2022-04-06 01:28:57 +0200

    Add files via upload

---
+++ tests/init_dev_tests.py
@@ -0,0 +1,23 @@
+''' Dev tests '''
+
+import numpy as np
+import sciris as sc
+import pylab as pl
+import sys
+import os
+
+# Add module to paths
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+
+
+if __name__ == '__main__':
+
+    from hpvsim.sim import Sim
+    pars = {}#'network':'basic'}
+    sim = Sim(label='test1', pars=pars)
+    print(sim.label)
+    sim.run()
+
+
