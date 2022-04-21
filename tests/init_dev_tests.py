a4f14f5c5d89ade424745b6dedd4c7289a632f5d
Robyn Stuart <robyna.s@gmail.com>
2022-04-21 18:59:46 +1000

    replace loop with array

--- tests/init_dev_tests.py
+++ tests/init_dev_tests.py
@@ -41,25 +41,38 @@ def test_genotypes():
         'dt': .1
     }
     sim = Sim(pars=pars)
-    sim.run()
 
-    fig, ax = pl.subplots(2, 1, figsize=(8, 12))
-    timevec = sim.results['year']
-    ax[0].plot(timevec, sim.results['n_alive'].values)
-    ax[0].set_title('Number of people alive')
-    for i, genotype in sim['genotype_map'].items():
-        ax[1].plot(timevec, sim.results['genotype']['new_infections_by_genotype'].values[i,:], label=genotype)
-    ax[1].legend()
-    ax[1].set_title('New infections by genotype')
-    fig.show()
-    return sim
+    # fig, ax = pl.subplots(2, 1, figsize=(8, 12))
+    # timevec = sim.results['year']
+    # ax[0].plot(timevec, sim.results['n_alive'].values)
+    # ax[0].set_title('Number of people alive')
+    # for i, genotype in sim['genotype_map'].items():
+    #     ax[1].plot(timevec, sim.results['genotype']['new_infections_by_genotype'].values[i,:], label=genotype)
+    # ax[1].legend()
+    # ax[1].set_title('New infections by genotype')
+    # fig.show()
+    # return sim
 
 
 if __name__ == '__main__':
 
     # sim0 = test_random() # NOT WORKING
     # sim1 = test_basic() # NOT WORKING
-    sim2 = test_genotypes()
+    # sim2 = test_genotypes()
+
+    from hpvsim.sim import Sim
+    from hpvsim.immunity import genotype
+
+    hpv16 = genotype('HPV16')
+    hpv18 = genotype('HPV18')
+    pars = {
+        'network': 'basic',
+        'genotypes': [hpv16, hpv18],
+        'dt': .1
+    }
+    sim = Sim(pars=pars)
+    sim.run()
+
 
 
 
