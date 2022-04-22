ead8e15e0a8978b16f598e2143afea948dfb77d1
Robyn Stuart <robyna.s@gmail.com>
2022-04-22 13:28:51 +1000

    start adding interventions

--- hpvsim/interventions.py
+++ hpvsim/interventions.py
@@ -14,7 +14,269 @@ import sciris as sc
 # from . import utils as cvu
 # from . import base as cvb
 # from . import defaults as cvd
-# from . import parameters as cvpar
+from . import parameters as hppar
 # from . import immunity as cvi
 # from collections import defaultdict
+
+
+#%% Generic intervention classes
+
+__all__ = ['Intervention']
+
+
+class Intervention:
+    '''
+    Base class for interventions.
+    Args:
+        label       (str): a label for the intervention (used for plotting, and for ease of identification)
+        show_label (bool): whether or not to include the label in the legend
+        do_plot    (bool): whether or not to plot the intervention
+        line_args  (dict): arguments passed to pl.axvline() when plotting
+    '''
+    def __init__(self, label=None, show_label=False, do_plot=None, line_args=None):
+        self._store_args() # Store the input arguments so the intervention can be recreated
+        if label is None: label = self.__class__.__name__ # Use the class name if no label is supplied
+        self.label = label # e.g. "Screen"
+        self.show_label = show_label # Do not show the label by default
+        self.do_plot = do_plot if do_plot is not None else True # Plot the intervention, including if None
+        self.line_args = sc.mergedicts(dict(linestyle='--', c='#aaa', lw=1.0), line_args) # Do not set alpha by default due to the issue of overlapping interventions
+        self.days = [] # The start and end days of the intervention
+        self.initialized = False # Whether or not it has been initialized
+        self.finalized = False # Whether or not it has been initialized
+        return
+
+
+    def __repr__(self, jsonify=False):
+        ''' Return a JSON-friendly output if possible, else revert to short repr '''
+
+        if self.__class__.__name__ in __all__ or jsonify:
+            try:
+                json = self.to_json()
+                which = json['which']
+                pars = json['pars']
+                parstr = ', '.join([f'{k}={v}' for k,v in pars.items()])
+                output = f"cv.{which}({parstr})"
+            except Exception as E:
+                output = f'{type(self)} (error: {str(E)})' # If that fails, print why
+            return output
+        else:
+            return f'{self.__module__}.{self.__class__.__name__}()'
+
+
+    def __call__(self, *args, **kwargs):
+        # Makes Intervention(sim) equivalent to Intervention.apply(sim)
+        if not self.initialized:  # pragma: no cover
+            errormsg = f'Intervention (label={self.label}, {type(self)}) has not been initialized'
+            raise RuntimeError(errormsg)
+        return self.apply(*args, **kwargs)
+
+
+    def disp(self):
+        ''' Print a detailed representation of the intervention '''
+        return sc.pr(self)
+
+
+    def _store_args(self):
+        ''' Store the user-supplied arguments for later use in to_json '''
+        f0 = inspect.currentframe() # This "frame", i.e. Intervention.__init__()
+        f1 = inspect.getouterframes(f0) # The list of outer frames
+        parent = f1[2].frame # The parent frame, e.g. change_beta.__init__()
+        _,_,_,values = inspect.getargvalues(parent) # Get the values of the arguments
+        if values:
+            self.input_args = {}
+            for key,value in values.items():
+                if key == 'kwargs': # Store additional kwargs directly
+                    for k2,v2 in value.items(): # pragma: no cover
+                        self.input_args[k2] = v2 # These are already a dict
+                elif key not in ['self', '__class__']: # Everything else, but skip these
+                    self.input_args[key] = value
+        return
+
+
+    def initialize(self, sim=None):
+        '''
+        Initialize intervention -- this is used to make modifications to the intervention
+        that can't be done until after the sim is created.
+        '''
+        self.initialized = True
+        self.finalized = False
+        return
+
+
+    def finalize(self, sim=None):
+        '''
+        Finalize intervention
+
+        This method is run once as part of `sim.finalize()` enabling the intervention to perform any
+        final operations after the simulation is complete (e.g. rescaling)
+        '''
+        if self.finalized: # pragma: no cover
+            raise RuntimeError('Intervention already finalized')  # Raise an error because finalizing multiple times has a high probability of producing incorrect results e.g. applying rescale factors twice
+        self.finalized = True
+        return
+
+
+    def apply(self, sim):
+        '''
+        Apply the intervention. This is the core method which each derived intervention
+        class must implement. This method gets called at each timestep and can make
+        arbitrary changes to the Sim object, as well as storing or modifying the
+        state of the intervention.
+
+        Args:
+            sim: the Sim instance
+
+        Returns:
+            None
+        '''
+        raise NotImplementedError
+
+
+    def shrink(self, in_place=False):
+        '''
+        Remove any excess stored data from the intervention; for use with sim.shrink().
+
+        Args:
+            in_place (bool): whether to shrink the intervention (else shrink a copy)
+        '''
+        if in_place: # pragma: no cover
+            return self
+        else:
+            return sc.dcp(self)
+
+
+    def plot_intervention(self, sim, ax=None, **kwargs):
+        '''
+        Plot the intervention
+
+        This can be used to do things like add vertical lines on days when
+        interventions take place. Can be disabled by setting self.do_plot=False.
+
+        Note 1: you can modify the plotting style via the ``line_args`` argument when
+        creating the intervention.
+
+        Note 2: By default, the intervention is plotted at the days stored in self.days.
+        However, if there is a self.plot_days attribute, this will be used instead.
+
+        Args:
+            sim: the Sim instance
+            ax: the axis instance
+            kwargs: passed to ax.axvline()
+
+        Returns:
+            None
+        '''
+        line_args = sc.mergedicts(self.line_args, kwargs)
+        if self.do_plot or self.do_plot is None:
+            if ax is None:
+                ax = pl.gca()
+            if hasattr(self, 'plot_days'):
+                days = self.plot_days
+            else:
+                days = self.days
+            if sc.isiterable(days):
+                label_shown = False # Don't show the label more than once
+                for day in days:
+                    if sc.isnumber(day):
+                        if self.show_label and not label_shown: # Choose whether to include the label in the legend
+                            label = self.label
+                            label_shown = True
+                        else:
+                            label = None
+                        date = sc.date(sim.date(day))
+                        ax.axvline(date, label=label, **line_args)
+        return
+
+
+    def to_json(self):
+        '''
+        Return JSON-compatible representation
+
+        Custom classes can't be directly represented in JSON. This method is a
+        one-way export to produce a JSON-compatible representation of the
+        intervention. In the first instance, the object dict will be returned.
+        However, if an intervention itself contains non-standard variables as
+        attributes, then its `to_json` method will need to handle those.
+
+        Note that simply printing an intervention will usually return a representation
+        that can be used to recreate it.
+
+        Returns:
+            JSON-serializable representation (typically a dict, but could be anything else)
+        '''
+        which = self.__class__.__name__
+        pars = sc.jsonify(self.input_args)
+        output = dict(which=which, pars=pars)
+        return output
+
+
+class dynamic_pars(Intervention):
+    '''
+    A generic intervention that modifies a set of parameters at specified points
+    in time.
+
+    The intervention takes a single argument, pars, which is a dictionary of which
+    parameters to change, with following structure: keys are the parameters to change,
+    then subkeys 'days' and 'vals' are either a scalar or list of when the change(s)
+    should take effect and what the new value should be, respectively.
+
+    You can also pass parameters to change directly as keyword arguments.
+
+    Args:
+        pars (dict): described above
+        kwargs (dict): passed to Intervention()
+
+    **Examples**::
+
+        interv = cv.dynamic_pars(condoms=dict(timepoints=10, vals={'c':0.9})) # Increase condom use amount casual partners to 90%
+        interv = cv.dynamic_pars({'beta':{'timepoints':[10, 15], 'vals':[0.005, 0.015]}, # At timepoint 10, reduce beta, then increase it again
+                                  'debut':{'timepoints':10, 'vals':dict(f=dict(dist='normal', par1=20, par2=2.1), m=dict(dist='normal', par1=19.6, par2=1.8))}}) # Increase mean age of sexual debut
+    '''
+
+    def __init__(self, pars=None, **kwargs):
+
+        # Find valid sim parameters and move matching keyword arguments to the pars dict
+        pars = sc.mergedicts(pars) # Ensure it's a dictionary
+        sim_par_keys = list(hppar.make_pars().keys()) # Get valid sim parameters
+        kwarg_keys = [k for k in kwargs.keys() if k in sim_par_keys]
+        for kkey in kwarg_keys:
+            pars[kkey] = kwargs.pop(kkey)
+
+        # Do standard initialization
+        super().__init__(**kwargs) # Initialize the Intervention object
+
+        # Handle the rest of the initialization
+        subkeys = ['timepoints', 'vals']
+        for parkey in pars.keys():
+            for subkey in subkeys:
+                if subkey not in pars[parkey].keys(): # pragma: no cover
+                    errormsg = f'Parameter {parkey} is missing subkey {subkey}'
+                    raise sc.KeyNotFoundError(errormsg)
+                if sc.isnumber(pars[parkey][subkey]): # Allow scalar values or dicts, but leave everything else unchanged
+                    pars[parkey][subkey] = sc.promotetoarray(pars[parkey][subkey])
+            timepoints = pars[parkey]['timepoints']
+            vals = pars[parkey]['vals']
+            if sc.isiterable(timepoints):
+                len_timepoints = len(timepoints)
+                len_vals = len(vals)
+                if len_timepoints != len_vals: # pragma: no cover
+                    raise ValueError(f'Length of timepoints ({len_timepoints}) does not match length of values ({len_vals}) for parameter {parkey}')
+        self.pars = pars
+        return
+
+
+    def apply(self, sim):
+        ''' Loop over the parameters, and then loop over the days, applying them if any are found '''
+        t = sim.t
+        for parkey,parval in self.pars.items():
+            if t in parval['timepoints']: # TODO: make this more robust
+                self.timepoints.append(t)
+                val = parval['vals'][ind]
+                if isinstance(val, dict):
+                    sim[parkey].update(val) # Set the parameter if a nested dict
+                else:
+                    sim[parkey] = val # Set the parameter if not a dict
+        return
+
+
 
