e0969ee7fb63ad50fa4473cb679eee6b6e319401
Jamie Cohen <jamie.alex.cohen@gmail.com>
2022-07-08 08:58:37 -0700

    fixed typo

--- hpvsim/sim.py
+++ hpvsim/sim.py
@@ -680,7 +680,7 @@ class Sim(hpb.BaseSim):
             latent_inds = hpu.true(people.latent[g,:])
             if len(latent_inds):
                 age_inds = np.digitize(people.age[latent_inds], self['hpv_reactivation']['age_cutoffs'])-1 # convert ages to indices
-                reactivation_probs = self['hpv_reactivation']['hpv_reactivation_prons'][age_inds]
+                reactivation_probs = self['hpv_reactivation']['hpv_reactivation_probs'][age_inds]
                 is_reactivated = hpu.binomial_arr(reactivation_probs)
                 reactivated_inds = latent_inds[is_reactivated]
                 people.infect(inds=reactivated_inds, g=g, layer='reactivation')
