c80c644ced3fffae0e02233acad2613a790c39ea
cliffckerr <questionably@gmail.com>
2022-07-18 15:05:58 +1000

    working on age data

--- data/get_age_data.py
+++ data/get_age_data.py
@@ -1,32 +1,47 @@
 ''' Import population sizes by age from UNPD '''
 
-import sciris as sc
-import numpy as np
+
 import os
-import urllib.request as urllib
 import zipfile
+import urllib.request as urllib
+import sciris as sc
 import pandas as pd
 
-# Filepaths
-urls = ["https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2022_Population1JanuaryBySingleAgeSex_Medium_1950-2021.zip",
-        "https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2022_Population1JanuaryBySingleAgeSex_Medium_2022-2100.zip"]
-local_paths = ["WPP2022_Population1JanuaryBySingleAgeSex_Medium_1950-2021.csv",
-               "WPP2022_Population1JanuaryBySingleAgeSex_Medium_2022-2100.csv"]
+local_stem = 'WPP2022_Population1JanuaryBySingleAgeSex_Medium_'
+url_stem = f'https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/{local_stem}'
+years = ['1950-2021', '2022-2100']
 
-# Download data if it's not already in the directory
-for url, local_path in zip(urls, local_paths):
-    if not os.path.exists(local_path):
-        print(f'Downloading from {url}, this may take a while...')
-        filehandle, _ = urllib.urlretrieve(url)
-        zip_file_object = zipfile.ZipFile(filehandle, 'r')
-        zip_file_object.extractall()
+def get_age_data(force=False, tidy=True):
+    sc.heading('Downloading age data...')
+    T = sc.timer()
+    dfs = []
 
-# Extract the parts used in the model and save
-df1 = pd.read_csv(local_paths[0])
-df2 = pd.read_csv(local_paths[1])
+    # Download data if it's not already in the directory
+    for year in years:
+        url = f'{url_stem}{year}.zip'
+        local_path = f'{local_stem}{year}.csv'
+        if force or not os.path.exists(local_path):
+            print(f'Downloading from {url}, this may take a while...')
+            filehandle, _ = urllib.urlretrieve(url)
+            zip_file_object = zipfile.ZipFile(filehandle, 'r')
+            zip_file_object.extractall()
 
-df11 = df1[["Location", "Time", "AgeGrpStart", "PopTotal"]]
-df22 = df2[["Location", "Time", "AgeGrpStart", "PopTotal"]]
-df = pd.concat([df11, df22])
-dd = {l:df[df["Location"]==l] for l in df["Location"].unique()}
-sc.save('populations.obj',dd)
+        # Extract the parts used in the model and save
+        df = pd.read_csv(local_path)
+        df = df[["Location", "Time", "AgeGrpStart", "PopTotal"]]
+        dfs.append(df)
+        if tidy:
+            os.remove(local_path)
+        T.toctic(label=f'Done with {year}')
+
+    df = pd.concat(dfs)
+    dd = {l:df[df["Location"]==l] for l in df["Location"].unique()}
+    sc.save('populations.obj',dd)
+
+
+    print(f'Done: took {T.timings[:].sum():0.1f} s.')
+
+    return dd
+
+if __name__ == '__main__':
+    dd = get_age_data()
