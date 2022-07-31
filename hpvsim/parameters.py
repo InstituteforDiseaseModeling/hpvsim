af03de61d37fab5e302647b732cf805a97579aab
Jamie Cohen <jamie.alex.cohen@gmail.com>
2022-07-30 21:15:25 -0700

    cross immunity in vaccines

--- hpvsim/parameters.py
+++ hpvsim/parameters.py
@@ -762,11 +762,11 @@ def get_vaccine_genotype_pars(default=False, vaccine=None):
         bivalent = dict(
             hpv16=1,
             hpv18=1,  # Assumption
-            hpv31=0,  # Assumption
-            hpv33=0,  # Assumption
-            hpv45=0,  # Assumption
-            hpv52=0,  # Assumption
-            hpv58=0,  # Assumption
+            hpv31=0.5,  # Assumption
+            hpv33=0.5,  # Assumption
+            hpv45=0.5,  # Assumption
+            hpv52=0.3,  # Assumption
+            hpv58=0.3,  # Assumption
             hpv6=0,  # Assumption
             hpv11=0,  # Assumption
         ),
