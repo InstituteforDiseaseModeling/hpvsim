1bc7a394da26548943f6499e215a53397eb55048
Robyn Stuart <robyna.s@gmail.com>
2022-08-25 11:42:15 +1000

    one more

--- tests/test_interventions.py
+++ tests/test_interventions.py
@@ -385,18 +385,18 @@ def test_screening_ltfu(do_plot=False, do_save=False, fig_path=None):
     # Model an intervention to screen 50% of 30 year olds with hpv DNA testing and treat immediately
     hpv_screening = hpv.Screening(primary_screen_test='hpv', treatment='via_triage', screen_start_age=30,
                                   screen_start_year=2020,
-                                  screen_stop_age=50, screen_interval=5, timepoints='2010',
+                                  screen_stop_age=50, screen_interval=5,
                                   screen_compliance=0.7, triage_compliance=0.9, cancer_compliance=0.2,
                                   excision_compliance=0.2, ablation_compliance=0.7)
 
 
     hpv_via_screening = hpv.Screening(primary_screen_test='hpv', triage_screen_test='via', treatment='via_triage', screen_start_age=30,
-                                  screen_stop_age=50, screen_interval=10, timepoints='2010', label='hpv primary, via triage',
+                                  screen_stop_age=50, screen_interval=10, label='hpv primary, via triage',
                                       screen_compliance=0.7, triage_compliance=0.9, cancer_compliance=0.2,
                                       excision_compliance=0.2, ablation_compliance=0.7,screen_start_year=2020,)
 
     hpv_via_screening_more_ltfu = hpv.Screening(primary_screen_test='hpv', triage_screen_test='via', treatment='via_triage', screen_start_age=30,
-                                  screen_stop_age=50, screen_interval=10, timepoints='2010', label='hpv primary, via triage, more LTFU',
+                                  screen_stop_age=50, screen_interval=10, label='hpv primary, via triage, more LTFU',
                                       screen_compliance=0.7, triage_compliance=0.6, cancer_compliance=0.2,
                                       excision_compliance=0.1, ablation_compliance=0.5,screen_start_year=2020,)
 
