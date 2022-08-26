ed985d520d847ff942913c7f15ee6d7bf217d9b2
Robyn Stuart <robyna.s@gmail.com>
2022-08-26 12:32:12 +1000

    tidying

--- tests/test_interventions.py
+++ tests/test_interventions.py
@@ -76,7 +76,7 @@ def test_complex_vax(do_plot=False, do_save=False, fig_path=None):
     interventions = [routine_vx, campaign_vx]
 
     n_runs = 1
-    sim = hpv.Sim(pars=base_pars, interventions=interventions)
+    sim = hpv.Sim(pars=base_pars)
 
     # Define the scenarios
     scenarios = {
@@ -453,14 +453,12 @@ if __name__ == '__main__':
     # Start timing and optionally enable interactive plotting
     T = sc.tic()
 
-    # sim0 = test_dynamic_pars()
-    # scens0 = test_complex_vax(do_plot=True)
-    sim = test_complex_vax(do_plot=True)
-
-    # scens1 = test_vaccinate_prob(do_plot=True)
-    # scens2 = test_vaccinate_num(do_plot=True)
-    # scens3 = test_screening(do_plot=True)
-    # scens4 = test_screening_ltfu(do_plot=True)
+    sim0 = test_dynamic_pars()
+    scens0 = test_complex_vax(do_plot=True)
+    scens1 = test_vaccinate_prob(do_plot=True)
+    scens2 = test_vaccinate_num(do_plot=True)
+    scens3 = test_screening(do_plot=True)
+    # scens4 = test_screening_ltfu(do_plot=True) # CURRENTLY BROKEN
 
     sc.toc(T)
     print('Done.')
