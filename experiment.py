a5386a7ee3256b43b1cf5a1a5ccffc1065441574
Romesh Abeysuriya <romesh.abey@gmail.com>
2022-10-10 17:12:45 +1100

    Next version

--- experiment.py
+++ experiment.py
@@ -12,36 +12,42 @@ import hpvsim.utils as hpu
 
 n = 1000
 npts = 300
-dt = 0.1
-scale = 100
-scale_can = 1
+dt = 0.25
 
-recover_prob = 1 / 10
-can_prob = 0.001
-death_prob = 0.1
-beta = 0.15
+scale = 100 # people per agent for non-cancer and non-death states
+scale_can = 1 # people per agent for cancer and death states
+
+can_prob = 0.005 # probability of getting cancer if infected
+time_to_rec = dict(dist='lognormal', par1=2.5, par2=4.0) # average infection duration, given recovery
+time_to_can = dict(dist='lognormal', par1=5.5, par2=3.0) # average time to cancer
+beta = 0.11 # transmission scale factor
 
 class People():
-    people_keys = ['age', 'sus', 'inf', 'can', 'dead', 'scale']
+    people_keys = ['age', 'sus', 'inf', 'can', 'scale']
 
-    def __init__(self):
+    def __init__(self, n_infected):
 
+        self.ti = 0
         self.age = np.random.uniform(20, 40, size=n)
 
         arr = lambda val=None: np.full(n, val)
         self.sus = arr(True)
         self.inf = arr(False)
         self.can = arr(False)
-        self.dead = arr(False)
         self.scale = arr(scale)
+        self.date_rec = arr(np.nan)
+        self.date_can = arr(np.nan)
 
         self.res = sc.objdict()
-        for k in ['n_sus', 'n_inf', 'n_can', 'n_dead', 'new_inf', 'new_can', 'new_dead', 'cum_inf', 'cum_can', 'cum_dead', 'alive','age']:
+        for k in ['n_sus', 'n_inf', 'n_can', 'new_inf', 'new_rec', 'new_can', 'cum_inf', 'cum_can', 'alive','age']:
             self.res[k + '_agents'] = np.zeros(npts)
             self.res[k + '_people'] = np.zeros(npts)
 
-        self.sus[0:10] = False
-        self.inf[0:10] = True
+        self.infect(np.arange(0, n_infected))
+
+    @property
+    def t(self):
+        return self.ti*dt
 
     def extend(self, inds):
         for key in self.people_keys:
@@ -52,85 +58,99 @@ class People():
         new_inds = np.arange(orig_len, new_len)
         return new_inds
 
-    def update(self, ti):
+    def step(self):
 
         self.age += dt
 
-        self.step_transmission(ti)
-        self.step_cancer(ti)
-        self.update_results(ti)
+        new_rec = self.check_recovery()
+        self.res['new_rec_agents'][self.ti] = len(new_rec)
+        self.res['new_rec_people'][self.ti] = len(new_rec) * scale
 
-    def step_transmission(self, ti):
+        new_inf = self.check_transmission()
+        self.res['new_inf_agents'][self.ti] = len(new_inf)
+        self.res['new_inf_people'][self.ti] = len(new_inf) * scale
+
+        self.infect(new_inf)
+
+        new_can = self.check_cancer()
+        self.res['new_can_agents'][self.ti] = len(new_can)
+        self.res['new_can_people'][self.ti] = len(new_can) * scale
+
+        self.update_results()
+
+        self.ti += 1
+
+    def check_transmission(self):
+        inds = np.random.choice(hpu.true(self.sus), hpu.poisson(np.count_nonzero(self.inf) * beta))
+        return inds
+
+    def infect(self, inds):
         # Calculate transmission
-        new_inf = np.random.choice(hpu.true(self.sus), hpu.poisson(np.count_nonzero(self.inf) * beta))
-        self.sus[new_inf] = False
-        self.inf[new_inf] = True
-        self.res['new_inf_agents'][ti] = len(new_inf)
-        self.res['new_inf_people'][ti] = len(new_inf) * scale
+        self.sus[inds] = False
+        self.inf[inds] = True
 
-        # Calculate recovery
-        recover_inds = hpu.binomial_filter(recover_prob, hpu.true(self.inf))
-        self.sus[recover_inds] = True
-        self.inf[recover_inds] = False
+        # Assign everyone a recovery date
+        self.date_rec[inds] = self.t + hpu.sample(**time_to_rec, size=len(inds))
 
-    def step_cancer(self, ti):
-        # Calculate cancer
-        new_can = hpu.binomial_filter(can_prob, hpu.true(self.inf))
+        # Who has cancer?
+        can_inds = hpu.binomial_filter(can_prob, inds)
+        self.date_rec[can_inds] = np.nan
+        self.date_can[can_inds] = self.t + hpu.sample(**time_to_can, size=len(can_inds))
+
+
+    def check_recovery(self):
+        new_rec =  hpu.true(self.inf & (self.t >= self.date_rec))
+        self.sus[new_rec] = True
+        self.inf[new_rec] = False
+        return new_rec
+
+    def check_cancer(self):
+        new_can =  hpu.true(self.inf & (self.t >= self.date_can))
         self.inf[new_can] = False
         self.can[new_can] = True
-        self.res['new_can_agents'][ti] = len(new_can)
-        self.res['new_can_people'][ti] = len(new_can) * scale
+        return new_can
 
-        # Calculate death
-        new_dead = hpu.binomial_filter(death_prob, hpu.true(self.can))
-        self.can[new_dead] = False
-        self.dead[new_dead] = True
-        self.res['new_dead_agents'][ti] = len(new_dead)
-        self.res['new_dead_people'][ti] = len(new_dead) * scale
-
-    def update_results(self, ti):
+    def update_results(self):
 
         self.res['t'] = np.arange(0, npts*dt, dt)
-        self.res['n_sus_agents'][ti] = (self.sus).sum()
-        self.res['n_sus_people'][ti] = (self.sus * self.scale).sum()
-        self.res['n_inf_agents'][ti] = (self.inf).sum()
-        self.res['n_inf_people'][ti] = (self.inf * self.scale).sum()
-        self.res['n_can_agents'][ti] = (self.can).sum()
-        self.res['n_can_people'][ti] = (self.can * self.scale).sum()
-        self.res['n_dead_agents'][ti] = (self.dead).sum()
-        self.res['n_dead_people'][ti] = (self.dead * self.scale).sum()
+        self.res['n_sus_agents'][self.ti] = (self.sus).sum()
+        self.res['n_sus_people'][self.ti] = (self.sus * self.scale).sum()
+        self.res['n_inf_agents'][self.ti] = (self.inf).sum()
+        self.res['n_inf_people'][self.ti] = (self.inf * self.scale).sum()
+        self.res['n_can_agents'][self.ti] = (self.can).sum()
+        self.res['n_can_people'][self.ti] = (self.can * self.scale).sum()
 
-        self.res['alive_agents'][ti] = (~self.dead).sum()
-        self.res['alive_people'][ti] = ((~self.dead) * self.scale).sum()
-        self.res['age_agents'][ti] = np.average(self.age, weights=(~self.dead).astype(float))
-        self.res['age_people'][ti] = np.average(self.age, weights=(~self.dead).astype(float)*self.scale)
 
-        if ti == npts - 1:
+        self.res['alive_agents'][self.ti] = len(self.sus)
+        self.res['alive_people'][self.ti] = self.scale.sum()
+        self.res['age_agents'][self.ti] = np.average(self.age)
+        self.res['age_people'][self.ti] = np.average(self.age, weights=self.scale)
+
+        if self.ti == npts - 1:
             self.res['cum_inf_agents'] = np.cumsum(self.res['new_inf_agents'])
             self.res['cum_inf_people'] = np.cumsum(self.res['new_inf_people'])
             self.res['cum_can_agents'] = np.cumsum(self.res['new_can_agents'])
             self.res['cum_can_people'] = np.cumsum(self.res['new_can_people'])
-            self.res['cum_dead_agents'] = np.cumsum(self.res['new_dead_agents'])
-            self.res['cum_dead_people'] = np.cumsum(self.res['new_dead_people'])
+
 
 class MultiScalePeople(People):
 
-    def step_cancer(self, ti):
+    def step_cancer(self):
 
 
         # Calculate cancer
         new_can = hpu.binomial_filter(can_prob, hpu.true(self.inf))
         self.inf[new_can] = False
         self.can[new_can] = True
-        self.res['new_can_agents'][ti] = len(new_can)
-        self.res['new_can_people'][ti] = len(new_can) * scale
+        self.res['new_can_agents'][self.ti] = len(new_can)
+        self.res['new_can_people'][self.ti] = len(new_can) * scale
 
         # Calculate death
         new_dead = hpu.binomial_filter(death_prob, hpu.true(self.can))
         self.can[new_dead] = False
         self.dead[new_dead] = True
-        self.res['new_dead_agents'][ti] = len(new_dead)
-        self.res['new_dead_people'][ti] = len(new_dead) * scale
+        self.res['new_dead_agents'][self.ti] = len(new_dead)
+        self.res['new_dead_people'][self.ti] = len(new_dead) * scale
 
 
 
@@ -138,7 +158,6 @@ class Sim(sc.prettyobj):
 
     def __init__(self, people):
         self.people = people
-        self.ti = 0
 
     @property
     def results(self):
@@ -149,9 +168,9 @@ class Sim(sc.prettyobj):
         return sc.dataframe(self.results)
 
     def run(self):
-        for ti in range(npts):
-            self.ti += dt
-            self.people.update(ti)
+        for i in range(npts):
+            self.people.step()
+
     #
     # # def plot(self):
     #     fig = plt.figure(figsize=(14, 10))
@@ -165,7 +184,7 @@ class Sim(sc.prettyobj):
     #     return fig
 
 
-sim = Sim(People())
+sim = Sim(People(n_infected=100))
 sim.run()
 # fig = sim.plot()
 
@@ -177,3 +196,45 @@ for i, key in enumerate(sim.df.columns):
         plt.title(key)
 
 plt.show()
+
+
+
+
+#######################
+
+
+#
+# dur_to_peak_dys = sample(**dur_dyps, size=len(cin1_inds))
+# prog_rate = genotype_pars[genotype_map[g]]['prog_rate']
+# prog_time = genotype_pars[genotype_map[g]]['prog_time']
+# mean_peaks = logf2(dur_to_peak_dys, prog_time, prog_rate)  # Apply a function that maps durations + genotype-specific progression to
+#
+# pars['severity_dist'] = dict(dist='lognormal', par1=None, par2=0.1) # Distribution of individual disease severity. Par1 is set to None because the mean is determined as a function of genotype and disease duration
+#
+# sev_dist = people.pars['severity_dist']['dist']
+# sev_par2 = people.pars['severity_dist']['par2']
+# peaks = np.minimum(1, sample(dist=sev_dist, par1=mean_peaks,par2=sev_par2))  # Evaluate peak dysplasia, which is a proxy for the clinical classification
+#
+# n = 100
+#
+# dur_dysp     = dict(dist='lognormal', par1=4.5, par2=4.0) # PLACEHOLDERS; INSERT SOURCE
+# dur_to_peak_dys = hpu.sample(**dur_dysp, size=n)
+#
+# prog_rate    = 0.79 # Rate of progression of dysplasia once it is established. This parameter is used as the growth rate within a logistic function that maps durations to progression probabilities
+# prog_time    = 4.4  # Point of inflection in logistic function
+# mean_peaks = logf2(dur_to_peak_dys, prog_time, prog_rate)  # Apply a function that maps durations + genotype-specific progression to
+#
+# We want the INVERSE of this. Distribution of dur_dysp given that the peak is greater than cin3?
+#
+# def logf2(x, c, k):
+#     '''
+#     Logistic function, constrained to pass through 0,0 and with upper asymptote
+#     at 1. Accepts 2 parameters: growth rate and point of inflexion.
+#     '''
+#     l_asymp = -1/(1+np.exp(k*c))
+#     return l_asymp + 1/( 1 + np.exp(-k*(x-c)))
+#
+#
+# def cutoff(t,c,k):
+#     z = -1/(1+np.exp(k*c))
+#     return (np.log(1/(t-z)-1)-k*c)/(-k)
