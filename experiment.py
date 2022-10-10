be191fbae1faef3248d2c5594c7fa1b699cb366b
Romesh Abeysuriya <romesh.abey@gmail.com>
2022-10-11 07:58:49 +1100

    WIP

--- experiment.py
+++ experiment.py
@@ -9,6 +9,7 @@ import sciris as sc
 import matplotlib.pyplot as plt
 # import hpvsim as hpv
 import hpvsim.utils as hpu
+from collections import Counter
 
 n = 1000
 npts = 300
@@ -17,26 +18,29 @@ dt = 0.25
 scale = 100 # people per agent for non-cancer and non-death states
 scale_can = 1 # people per agent for cancer and death states
 
-can_prob = 0.005 # probability of getting cancer if infected
+can_prob = 0.05 # probability of getting cancer if infected
 time_to_rec = dict(dist='lognormal', par1=2.5, par2=4.0) # average infection duration, given recovery
 time_to_can = dict(dist='lognormal', par1=5.5, par2=3.0) # average time to cancer
 beta = 0.11 # transmission scale factor
 
-class People():
-    people_keys = ['age', 'sus', 'inf', 'can', 'scale']
+class People(sc.objdict):
+    people_keys = ['age', 'sus', 'inf', 'can', 'scale', 'date_rec', 'date_can', 'date_remove', 'removed']
 
     def __init__(self, n_infected):
 
         self.ti = 0
-        self.age = np.random.uniform(20, 40, size=n)
-
         arr = lambda val=None: np.full(n, val)
+
+
+        self.age = np.random.uniform(20, 40, size=n)
         self.sus = arr(True)
         self.inf = arr(False)
         self.can = arr(False)
         self.scale = arr(scale)
         self.date_rec = arr(np.nan)
         self.date_can = arr(np.nan)
+        self.date_remove = arr(np.nan)
+        self.removed = arr(False)
 
         self.res = sc.objdict()
         for k in ['n_sus', 'n_inf', 'n_can', 'new_inf', 'new_rec', 'new_can', 'cum_inf', 'cum_can', 'alive','age']:
@@ -49,10 +53,10 @@ class People():
     def t(self):
         return self.ti*dt
 
-    def extend(self, inds):
+    def extend(self, ind, n_copies):
         for key in self.people_keys:
             orig_len = len(self[key])
-            to_copy = self[key][inds]
+            to_copy = [self[key][ind]]*n_copies
             self[key] = sc.cat(self[key], to_copy)
             new_len = len(self[key])
         new_inds = np.arange(orig_len, new_len)
@@ -74,7 +78,9 @@ class People():
 
         new_can = self.check_cancer()
         self.res['new_can_agents'][self.ti] = len(new_can)
-        self.res['new_can_people'][self.ti] = len(new_can) * scale
+        self.res['new_can_people'][self.ti] = self.scale[new_can].sum()
+
+        self.check_removed()
 
         self.update_results()
 
@@ -97,6 +103,15 @@ class People():
         self.date_rec[can_inds] = np.nan
         self.date_can[can_inds] = self.t + hpu.sample(**time_to_can, size=len(can_inds))
 
+    def remove(self, inds):
+        self.sus[inds] = False
+        self.inf[inds] = False
+        self.can[inds] = False
+        self.removed[inds] = True
+
+    def check_removed(self):
+        new_removed =  hpu.true(~self.removed & (self.t >= self.date_remove))
+        self.remove(new_removed)
 
     def check_recovery(self):
         new_rec =  hpu.true(self.inf & (self.t >= self.date_rec))
@@ -120,9 +135,11 @@ class People():
         self.res['n_can_agents'][self.ti] = (self.can).sum()
         self.res['n_can_people'][self.ti] = (self.can * self.scale).sum()
 
+        self.res['age_agents'][self.ti] = np.average(self.age, weights=(~self.removed).astype(float))
+        self.res['age_people'][self.ti] = np.average(self.age, weights=(~self.removed).astype(float)*self.scale)
 
-        self.res['alive_agents'][self.ti] = len(self.sus)
-        self.res['alive_people'][self.ti] = self.scale.sum()
+        self.res['alive_agents'][self.ti] = (~self.removed).sum()
+        self.res['alive_people'][self.ti] = self.scale[~self.removed].sum()
         self.res['age_agents'][self.ti] = np.average(self.age)
         self.res['age_people'][self.ti] = np.average(self.age, weights=self.scale)
 
@@ -132,26 +149,59 @@ class People():
             self.res['cum_can_agents'] = np.cumsum(self.res['new_can_agents'])
             self.res['cum_can_people'] = np.cumsum(self.res['new_can_people'])
 
+    def treat(self, inds):
+        inds = inds[~(self.removed[inds] | self.can[inds]) ]
+        self.sus[inds] = True
+        self.inf[inds] = False
+        self.date_can[inds] = np.nan
+        return inds
+
 
 class MultiScalePeople(People):
 
-    def step_cancer(self):
+    def __init__(self, *args, **kwargs):
+        self.cancer_queue = [] # contains (time to create, index to create from)
+        super().__init__(*args, **kwargs)
 
+    def infect(self, inds):
+        # Calculate transmission
+        self.sus[inds] = False
+        self.inf[inds] = True
 
-        # Calculate cancer
-        new_can = hpu.binomial_filter(can_prob, hpu.true(self.inf))
-        self.inf[new_can] = False
-        self.can[new_can] = True
-        self.res['new_can_agents'][self.ti] = len(new_can)
-        self.res['new_can_people'][self.ti] = len(new_can) * scale
+        # Assign everyone a recovery date
+        self.date_rec[inds] = self.t + hpu.sample(**time_to_rec, size=len(inds))
 
-        # Calculate death
-        new_dead = hpu.binomial_filter(death_prob, hpu.true(self.can))
-        self.can[new_dead] = False
-        self.dead[new_dead] = True
-        self.res['new_dead_agents'][self.ti] = len(new_dead)
-        self.res['new_dead_people'][self.ti] = len(new_dead) * scale
+        # Pick which agents will give rise to cancers - remove the scaled agent when they progress
+        can_inds = hpu.binomial_filter(can_prob, inds)
+        self.date_rec[can_inds] = np.nan
+        self.date_remove[can_inds] = self.t + hpu.sample(**time_to_can, size=len(can_inds))
 
+        # Queue creation of cancer agents
+        for ind in inds:
+            n_cancers = np.random.binomial(int(np.round(scale/scale_can)),can_prob)
+            date_can  = self.t + hpu.sample(**time_to_can, size=n_cancers)
+            self.cancer_queue.extend([(d,ind) for d in date_can])
+
+    def check_cancer(self):
+        t = self.t
+        cancers_today = Counter([x[1] for x in self.cancer_queue if x[0] >= t])
+        new_can = []
+        for ind, n in cancers_today.items():
+            inds = self.extend(ind, n)
+            new_can.extend(inds)
+            self.sus[inds] = False
+            self.removed[inds] = False
+            self.inf[inds] = False
+            self.can[inds] = True
+            self.scale[inds] = scale_can
+            self.date_remove[inds] = np.nan
+        self.cancer_queue = [x for x in self.cancer_queue if x[0] < t]
+        return new_can
+
+    def treat(self, inds):
+        inds = super().treat(inds)
+        inds = set(inds)
+        self.cancer_queue = [x for x in self.cancer_queue if x[1] not in inds]
 
 
 class Sim(sc.prettyobj):
@@ -171,33 +221,33 @@ class Sim(sc.prettyobj):
         for i in range(npts):
             self.people.step()
 
-    #
-    # # def plot(self):
-    #     fig = plt.figure(figsize=(14, 10))
-    #     for i, key in enumerate(sim.df.columns):
-    #         if key != 't':
-    #             plt.subplot(2, 4, i)
-    #             plt.plot(sim.df.t, sim.df[key])
-    #             plt.title(key)
-    #
-    #     plt.show()
-    #     return fig
+    def plot(self, fig=None, color='b'):
+        if fig is None:
+            fig = plt.figure(figsize=(14, 10))
+        else:
+            plt.figure(fig)
+
+        for i, key in enumerate(sim.df.columns):
+            if key != 't':
+                plt.subplot(5, 5, i + 1)
+                plt.plot(sim.df.t, sim.df[key], color=color)
+                plt.title(key)
+        plt.tight_layout()
+        return fig
 
 
 sim = Sim(People(n_infected=100))
 sim.run()
-# fig = sim.plot()
+fig = sim.plot()
 
-fig = plt.figure(figsize=(14, 10))
-for i, key in enumerate(sim.df.columns):
-    if key != 't':
-        plt.subplot(5, 5, i+1)
-        plt.plot(sim.df.t, sim.df[key])
-        plt.title(key)
-
-plt.show()
+sim = Sim(MultiScalePeople(n_infected=100))
+sim.run()
+sim.plot(fig, 'r')
 
 
+sim = Sim(People(n_infected=100))
+sim.run()
+fig = sim.plot()
 
 
 #######################
