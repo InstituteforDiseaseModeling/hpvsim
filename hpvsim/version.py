7afff2bd52702c9c1f331a1aaea38ed7c5b5f5ec
Robyn Stuart <robyna.s@gmail.com>
2022-10-17 13:24:41 +1100

    update baselines, changelog, version

--- hpvsim/version.py
+++ hpvsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.2.7'
-__versiondate__ = '2022-10-14'
+__version__ = '0.2.8'
+__versiondate__ = '2022-10-17'
 __license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2022 by IDM'
