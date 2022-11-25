7dc43b9b19ef12233b96b11dcb2d35fec25d1953
Robyn Stuart <robyna.s@gmail.com>
2022-11-25 19:41:20 +1100

    increment version

--- hpvsim/version.py
+++ hpvsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.4.2'
-__versiondate__ = '2022-11-21'
+__version__ = '0.4.3'
+__versiondate__ = '2022-11-24'
 __license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2022 by IDM'
