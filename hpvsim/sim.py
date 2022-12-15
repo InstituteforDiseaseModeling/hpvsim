285038a2f2e32a34fbc3d455e5b9b367af0e3726
Romesh Abeysuriya <romesh.abey@gmail.com>
2022-12-15 17:26:02 +1100

    Add back extra initialize to support specific workflow in the test

--- hpvsim/sim.py
+++ hpvsim/sim.py
@@ -86,6 +86,7 @@ class Sim(hpb.BaseSim):
         self.init_immunity() # initialize information about immunity
         self.init_people(reset=reset, init_states=init_states, **kwargs) # Create all the people (the heaviest step)
         self.init_analyzers()  # ...and the analyzers...
+        self.set_seed() # Reset the random seed again so the random number stream is consistent
         self.initialized   = True
         self.complete      = False
         self.results_ready = False
