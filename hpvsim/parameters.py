811886c08be7d24152f9e0939e8c92b85ae8e01a
Robyn Stuart <robyna.s@gmail.com>
2022-12-22 11:18:33 +1100

    remove use_multiscale

--- hpvsim/parameters.py
+++ hpvsim/parameters.py
@@ -33,7 +33,6 @@ def make_pars(**kwargs):
     pars['n_agents']        = 20e3      # Number of agents
     pars['total_pop']       = None      # If defined, used for calculating the scale factor
     pars['pop_scale']       = None      # How much to scale the population
-    pars['use_multiscale']  = False     # Whether to use multiscale modeling
     pars['ms_agent_ratio']  = 1         # Ratio of scale factor of cancer agents to normal agents -- must be an integer
     pars['network']         = 'default' # What type of sexual network to use -- 'random', 'basic', other options TBC
     pars['location']        = None      # What location to load data from -- default Seattle
