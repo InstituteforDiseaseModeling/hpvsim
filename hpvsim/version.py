2605feb851e33309650d9b7b0622ac8af2f55307
cliffckerr <questionably@gmail.com>
2023-01-30 11:46:52 -0500

    update version and changelog

--- hpvsim/version.py
+++ hpvsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.4.15'
-__versiondate__ = '2023-01-13'
+__version__ = '0.4.16'
+__versiondate__ = '2023-01-30'
 __license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2023 by IDM'
