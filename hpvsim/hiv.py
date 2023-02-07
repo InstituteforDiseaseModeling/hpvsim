592f142f777acae242a64d3754d5b23d98928ddc
jamiecohen <jamie.alex.cohen@gmail.com>
2023-02-07 15:39:26 -0800

    inherit from pars object

--- hpvsim/hiv.py
+++ hpvsim/hiv.py
@@ -11,7 +11,7 @@ from . import base as hpb
 from .data import loaders as hpdata
 
 
-class HIVPars(hpb.FlexPretty):
+class HIVPars(hpb.ParsObj):
     '''
         A class based around performing operations on a self.pars dict.
         '''
@@ -19,47 +19,6 @@ class HIVPars(hpb.FlexPretty):
     def __init__(self, location, art_datafile, hiv_datafile):
         pars = self.load_data(location=location, hiv_datafile=hiv_datafile, art_datafile=art_datafile)
         self.update_pars(pars, create=True)
-        return
-
-    def __getitem__(self, key):
-        ''' Allow sim['par_name'] instead of sim.pars['par_name'] '''
-        try:
-            return self.pars[key]
-        except:
-            all_keys = '\n'.join(list(self.pars.keys()))
-            errormsg = f'Key "{key}" not found; available keys:\n{all_keys}'
-            raise sc.KeyNotFoundError(errormsg)
-
-    def __setitem__(self, key, value):
-        ''' Ditto '''
-        if key in self.pars:
-            self.pars[key] = value
-        else:
-            all_keys = '\n'.join(list(self.pars.keys()))
-            errormsg = f'Key "{key}" not found; available keys:\n{all_keys}'
-            raise sc.KeyNotFoundError(errormsg)
-        return
-
-    def update_pars(self, pars=None, create=False):
-        '''
-        Update internal dict with new pars.
-
-        Args:
-            pars (dict): the parameters to update (if None, do nothing)
-            create (bool): if create is False, then raise a KeyNotFoundError if the key does not already exist
-        '''
-        if pars is not None:
-            if not isinstance(pars, dict):
-                raise TypeError(f'The pars object must be a dict; you supplied a {type(pars)}')
-            if not hasattr(self, 'pars'):
-                self.pars = pars
-            if not create:
-                available_keys = list(self.pars.keys())
-                mismatches = [key for key in pars.keys() if key not in available_keys]
-                if len(mismatches):
-                    errormsg = f'Key(s) {mismatches} not found; available keys are {available_keys}'
-                    raise sc.KeyNotFoundError(errormsg)
-            self.pars.update(pars)
         return
 
     # %% HIV methods
@@ -211,7 +170,7 @@ class HIVPars(hpb.FlexPretty):
 
         return hiv_incidence_rates, art_adherence
 
-    def load_data(self, location=None, hiv_datafile=None, art_datafile=None, **kwargs):
+    def load_data(self, location=None, hiv_datafile=None, art_datafile=None):
         ''' Load any data files that are used to create additional parameters, if provided '''
         hiv_data = sc.objdict()
         hiv_data.infection_rates, hiv_data.art_adherence = self.get_hiv_data(location=location, hiv_datafile=hiv_datafile, art_datafile=art_datafile)
