20df5bcead9f740708973837bac2e4926e0f56d7
jamiecohen <jamie.alex.cohen@gmail.com>
2023-03-05 15:39:43 -0800

    overcounting hiv impact on HPV

--- hpvsim/hiv.py
+++ hpvsim/hiv.py
@@ -294,7 +294,7 @@ class HIVsim(hpb.ParsObj):
             hiv_inds += list(inds)
             if len(inds):
                 for ir, rel_par in enumerate(['rel_sus', 'rel_sev', 'rel_imm']):
-                    people[rel_par][inds] *= self['hiv_pars'][rel_par][cd4state]
+                    people[rel_par][inds] = self['hiv_pars'][rel_par][cd4state]
 
         # If anyone has HIV, update their HPV parameters
         if len(hiv_inds):
