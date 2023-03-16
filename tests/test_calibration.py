cd70e3286b5e4cebc2eb9fe4d80a30d016a7ad1d
Robyn Stuart <robyna.s@gmail.com>
2023-03-17 10:36:00 +1100

    check closeness of sim and calib results

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -5,6 +5,7 @@ Test calibration
 #%% Imports and settings
 import sciris as sc
 import hpvsim as hpv
+import numpy as np
 
 do_plot = 1
 do_save = 0
@@ -17,6 +18,10 @@ def test_calibration():
     sc.heading('Testing calibration')
 
     pars = dict(n_agents=n_agents, start=1980, end=2020, dt=0.25, location='south africa')
+    pars['age_bins']  = np.array([ 0., 20., 25., 30., 40., 45., 50., 55., 65., 100])
+    pars['standard_pop']    = np.array([pars['age_bins'],
+                                 [.4, .08, .08, .12, .06, .06, .05, .07, .08, 0]])
+
     sim = hpv.Sim(pars)
     calib_pars = dict(
         beta=[0.05, 0.010, 0.20],
@@ -39,15 +44,23 @@ def test_calibration():
                                 'test_data/south_africa_cancer_data_2020.csv',
                             ],
                             extra_sim_results=extra_sim_results,
-                            total_trials=3, n_workers=1)
+                            total_trials=2, n_workers=1)
     calib.calibrate(die=True)
     calib.plot(res_to_plot=4)
 
+    # Make sure that rerunning the sims with the best pars from the calibration gives the same results
     sim = hpv.Sim(pars)
     calib_pars = calib.trial_pars_to_sim_pars(which_pars=0)
     sim.initialize()
     sim.update_pars(calib_pars)
     sim.run().plot()
+
+    # Check sim results against stored results in calib
+    yind = sc.findinds(sim.results['year'], 2010)[0]
+    sim_hpv_results = sim.results['hpv_prevalence_by_age'][:,yind]
+    calib_hpv_results = calib.analyzer_results[0]['hpv_prevalence']['2010.0']
+    assert np.allclose(sim_hpv_results, calib_hpv_results)
+
     return sim, calib
 
 
@@ -56,7 +69,7 @@ if __name__ == '__main__':
 
     T = sc.tic()
 
-    sim2, calib = test_calibration()
+    sim, calib = test_calibration()
 
     sc.toc(T)
     print('Done.')
