af798535281ce266b3dcdd6048932209c5c9aae4
cliffckerr <questionably@gmail.com>
2023-04-03 20:49:09 -0400

    try pandas limit

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         'numpy',
         'scipy',
-        'pandas>=1.4.0', 
+        'pandas>=1.4.0,<2.0.0', 
         'sciris>=2.0.4',
         'matplotlib',
         'seaborn',
