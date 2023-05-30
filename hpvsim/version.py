678c7fe8507b5f195c71fc5fa2944f755ae3d370
Robyn Stuart <robyna.s@gmail.com>
2023-05-31 09:45:03 +1000

    version

--- hpvsim/version.py
+++ hpvsim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '1.2.0'
-__versiondate__ = '2023-05-01'
+__versiondate__ = '2023-05-31'
 __license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2023 by IDM'
