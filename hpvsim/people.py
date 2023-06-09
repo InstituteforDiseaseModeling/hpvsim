bcfdf249a83dda14eaa1686d02b74f1a452810c6
jamiecohen <jamie.alex.cohen@gmail.com>
2023-06-09 10:10:07 -0700

    separate out set static and set network properties of agents

--- hpvsim/people.py
+++ hpvsim/people.py
@@ -694,7 +694,8 @@ class People(hpb.BasePeople):
 
         if new_births>0:
             # Generate other characteristics of the new people
-            uids, sexes, debuts, rel_sev, partners, geo = hppop.set_static(new_n=new_births, existing_n=len(self), pars=self.pars)
+            uids, sexes = hppop.set_static(new_n=new_births, existing_n=len(self))
+            debuts, rel_sev, partners, geo = hppop.set_network(new_n=new_births, sex=sexes, pars=self.pars)
             
             # Grow the arrays`
             new_inds = self._grow(new_births)
