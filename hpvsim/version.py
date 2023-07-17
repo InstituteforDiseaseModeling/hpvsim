5a6bb1ee03683f47c3fc9fb97c68929e513f03c6
jamiecohen <jamie.alex.cohen@gmail.com>
2023-07-17 11:24:01 -0700

    update version

--- hpvsim/version.py
+++ hpvsim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '1.2.2'
-__versiondate__ = '2023-07-12'
+__versiondate__ = '2023-07-17'
 __license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2023 by IDM'
