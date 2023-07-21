6d6a7000ebda53c5c3b4bbea73709984a4c458fc
jamiecohen <jamie.alex.cohen@gmail.com>
2023-07-21 12:37:28 -0700

    do not count reactivations as new infections

--- hpvsim/people.py
+++ hpvsim/people.py
@@ -851,7 +851,7 @@ class People(hpb.BasePeople):
         self.inactive[g, inds]      = False  # no longer inactive
 
         # Add to flow results. Note, we only count these infectious in the results if they happened at this timestep
-        if layer != 'seed_infection':
+        if layer != 'seed_infection' and layer !='reactivation':
             # Create overall flows
             self.flows['infections']                += self.scale_flows(inds) # Add the total count to the total flow data
             self.genotype_flows['infections'][g]    += self.scale_flows(inds) # Add the count by genotype to the flow data
