bcce8bc8c5299a7b0f51459b09b82981d1e93a95
Robyn Stuart <robyna.s@gmail.com>
2023-08-31 07:42:14 +1000

    fix tutorial

--- tests/devtests/test_multiscale_pars.py
+++ tests/devtests/test_multiscale_pars.py
@@ -74,16 +74,42 @@ sc.heading('CoVs')
 print(gc)
 
 quantity = ['infs', 'deaths'][1]
-fig = pl.figure()
+def set_font(size=None, font='Libertinus Sans'):
+    """ Set a custom font """
+    sc.fonts(add=sc.thisdir(aspath=True) / 'assets' / 'LibertinusSans-Regular.otf')
+    sc.options(font=font, fontsize=size)
+    return
+
+ut.set_font(size=16)
+fig, ax = plt.subplots(figsize=(11,9))
 index = gc.reset_index()
 colors = sc.vectocolor(pl.log(index['ms'].values), cmap='parula')
 sizes = 2*(index['n'].values)**(1/2)
-pl.scatter(gm['time'].values, gc[quantity].values, lw=0, marker='o', c=colors, s=sizes, alpha=0.7)
+scatter = pl.scatter(gm['time'].values, gc[quantity].values, lw=0, marker='o', c=colors, s=sizes, alpha=0.7)
 pl.gca().set_xscale('log')
 pl.gca().set_yscale('log')
+legend1 = pl.legend(*scatter.legend_elements(), loc="lower left", title="MS ratio")
+
+# handles, labels = scatter.legend_elements()
+# clabels = ['1', '3', '10', '30', '100']
+# handles, labels = sc.legend_elements(prop="sizes", alpha=0.6, num=10)
+# labels = ['100', '1k', '10k', '100k']
+# pl.legend(handles, labels, loc="upper right", title="# agents")
+
+msizes = [sizes[0], sizes[15], sizes[30], sizes[45]]
+labels = ['100', '1k', '10k', '100k']
+markers = []
+for sn,size in enumerate(msizes):
+   markers.append(pl.scatter([],[], s=size, c='k', alpha=0.6, label=labels[sn]))
+pl.legend(handles=markers, ncols=1, frameon=False, labelspacing=1.2, title="# agents", loc='upper right')
+ax.add_artist(legend1)
+# pl.legend(handles, clabels, loc="upper center", title="Multi-scale ratio")
+# pl.legend([l[0] for l in plot_lines], parameters, loc=4)
+# pl.gca().add_artist(legend1)
+
 pl.xlabel('Time per simulation (s)')
 pl.ylabel(f'Coefficient of variation in {quantity}')
-pl.title('Dot color = multiscale agent ratio\nDot size = number of agents')
+# pl.title('Dot color = multiscale agent ratio\nDot size = number of agents')
 pl.show()
 
 
