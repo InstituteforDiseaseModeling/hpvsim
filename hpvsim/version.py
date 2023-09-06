7aa4daf70d7bb5ba6f30fe61beaa330d6d47dd9d
Luojun Yang <l.yang1016@gmail.com>
2023-09-06 16:45:14 -0700

    Update version.py

--- hpvsim/version.py
+++ hpvsim/version.py
@@ -6,5 +6,4 @@ __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '1.2.3'
 __versiondate__ = '2023-08-30'
-
 __license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2023 by IDM'
