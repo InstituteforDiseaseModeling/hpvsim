7ea97e99db8ec888bb5f2f53314a2b0adc0e643b
serinlee <56375042+serinlee@users.noreply.github.com>
2023-10-13 14:05:58 -0700

    minor fix

--- tests/test_parameter_exploration.py
+++ tests/test_parameter_exploration.py
@@ -2,7 +2,7 @@
 import sciris as sc
 import hpvsim as hpv
 import optuna as op
-from tests.test_sampler import PredefinedSampler
+from .test_sampler import PredefinedSampler
 import numpy as np
 import pandas as pd
 import math
