2cc956292d57a2c6a57dc0d4a193f012a418d5de
Robyn Stuart <robyna.s@gmail.com>
2023-11-16 09:34:37 +1100

    nh looking good

--- tests/devtests/plot_nathx.py
+++ tests/devtests/plot_nathx.py
@@ -202,10 +202,10 @@ def plot_stacked(sim=None):
     df["prob_progressed"] = (res["progressed"]+ res["cancer"]) / total_alive * 100
 
     df2 = pd.DataFrame()
-    total_persisted_alive = res["total"] - res["dead"] - res["cleared"] -res["persisted"]
+    total_persisted = res["total"] - res["dead_other"] - res["cleared"]
     df2["years"] = years
-    df2["prob_progressed"] = (res["progressed"]) / total_persisted_alive * 100
-    df2["prob_cancer"] = (res["cancer"]) / total_persisted_alive * 100
+    df2["prob_persisted"] = (res["persisted"] + res["progressed"]) / total_persisted * 100
+    df2["prob_cancer"] = (res["cancer"]+ res["dead_cancer"]) / total_persisted * 100
 
     ####################
     # Make figure, set fonts and colors
@@ -230,7 +230,7 @@ def plot_stacked(sim=None):
 
     # Panel 2, conditional on being alive and not cleared
     bottom = np.zeros(len(df["years"]))
-    layers = ["prob_progressed", "prob_cancer"]
+    layers = ["prob_persisted", "prob_cancer"]
     labels = ["CIN2+ regression/persistence", "Cancer"]
     for ln, layer in enumerate(layers):
         axes[1].fill_between(
@@ -309,7 +309,7 @@ def plot_nh(sim=None):
 
         # Panel D: dysplasia
 
-        cancer = hppar.compute_severity(this_cinx[:], pars=cancer_fns[gi])
+        cancer = hppar.compute_severity(this_cinx[:], pars=sc.mergedicts(cin_fns[gi], cancer_fns[gi]))
         axes[3].plot(this_cinx, cancer, color=colors[gi], lw=2, label=gtype.upper())
 
 
@@ -439,7 +439,7 @@ def plot_nh_simple(sim=None):
         axes[2].plot(this_cinx, rv.pdf(this_cinx), color=colors[gi], lw=2, label=glabels[gi])
 
         # Panel D: cancer
-        cancer = hppar.compute_severity(this_cinx[:], pars=cancer_fns[gi])
+        cancer = hppar.compute_severity(this_cinx[:], pars=sc.mergedicts(cin_fns[gi], cancer_fns[gi]))
         axes[3].plot(this_cinx, cancer, color=colors[gi], lw=2, label=gtype.upper())
 
 
@@ -494,7 +494,7 @@ if __name__ == '__main__':
                 'end': 2020,
                 'ms_agent_ratio': 100,
                 'n_agents': 50e3,
-                'sev_dist': dict(dist='normal_pos', par1=1., par2=0.1)
+                # 'sev_dist': dict(dist='normal_pos', par1=1., par2=0.1)
             }
             age_causal_by_genotype = dwelltime_by_genotype(start_year=2000)
             outcomes_by_year = outcomes_by_year(start_year=2000)
