aa72f6143ebd8ab7002830e56a76b2ab4e5faa4e
Jen Schripsema <jen.schripsema@gatesfoundation.org>
2023-12-08 17:16:59 -0700

    removed README.md from doc build to resolve build issue

--- docs/conf.py
+++ docs/conf.py
@@ -120,7 +120,7 @@ release = hpv.__version__
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
 
 # suppress warnings for multiple possible Python references in the namespace
 # suppress_warnings = ['ref.python']
