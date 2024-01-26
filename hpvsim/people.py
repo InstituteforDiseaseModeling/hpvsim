dd49f73f40b2f55ec304030f314d1cfdd02b16be
jamiecohen <jamie.alex.cohen@gmail.com>
2024-01-26 06:27:27 -0800

    migration bug fix

--- hpvsim/people.py
+++ hpvsim/people.py
@@ -695,19 +695,25 @@ class People(hpb.BasePeople):
                 n_to_add = difference[ages_to_add] # Determine number of agents to add for each age
                 ages_to_add_list = np.repeat(ages_to_add, n_to_add)
 
-                if self.pars['model_hiv']:
-                    if sex == 'male':
-                        sex_attr = self.is_male
-                    else:
-                        sex_attr = self.is_female
-                    hivinds = hpu.true(self.hiv * self.alive * sex_attr)
-                    aliveinds = hpu.true(self.alive * sex_attr)
-                    hiv_prevalence = safedivide(np.histogram(self.age[hivinds], bins=np.arange(0, max(ages_to_add)+1), weights=self.scale[hivinds])[0],
-                                     np.histogram(self.age[aliveinds], bins=np.arange(0, max(ages_to_add)+1),weights=self.scale[aliveinds])[0])
-                    self.add_births(new_births=len(ages_to_add_list), ages=np.array(ages_to_add_list), sex_ratio=sex_ratio, hiv_prevalence=hiv_prevalence)
-                else:
-                    self.add_births(new_births=len(ages_to_add_list), ages=np.array(ages_to_add_list), sex_ratio=sex_ratio)
+                if len(ages_to_add):
 
+                    if self.pars['model_hiv']:
+                        if sex == 'male':
+                            sex_attr = self.is_male
+                        else:
+                            sex_attr = self.is_female
+                        hivinds = hpu.true(self.hiv * self.alive * sex_attr)
+                        aliveinds = hpu.true(self.alive * sex_attr)
+                        hiv_prevalence = safedivide(np.histogram(self.age[hivinds], bins=np.arange(0, max(ages_to_add) + 1),
+                                                                 weights=self.scale[hivinds])[0],
+                                                    np.histogram(self.age[aliveinds],
+                                                                 bins=np.arange(0, max(ages_to_add) + 1),
+                                                                 weights=self.scale[aliveinds])[0])
+                        self.add_births(new_births=len(ages_to_add_list), ages=np.array(ages_to_add_list),
+                                        sex_ratio=sex_ratio, hiv_prevalence=hiv_prevalence)
+
+                    else:
+                        self.add_births(new_births=len(ages_to_add_list), ages=np.array(ages_to_add_list), sex_ratio=sex_ratio)
 
                 # Remove people
                 if sex == 'male':
