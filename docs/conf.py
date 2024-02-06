df060df72f336f92baa50a0560ec89efafedbbe0
Cliff Kerr <questionably@gmail.com>
2024-02-05 23:19:55 -0500

    update year automatically

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import sciris as sc
 import hpvsim as hpv
 
 # Set environment
@@ -14,7 +15,7 @@ on_rtd = os.environ.get('READTHEDOCS') == 'True'
 # -- Project information -----------------------------------------------------
 
 project = 'HPVsim'
-copyright = f'2020 - 2023, Bill & Melinda Gates Foundation. All rights reserved.\nThese docs were built for HPVsim version {hpv.__version__}.\n'
+copyright = f'2020 - {sc.now().year}, Bill & Melinda Gates Foundation. All rights reserved.\nThese docs were built for HPVsim version {hpv.__version__}.\n'
 author = 'Institute for Disease Modeling'
 
 # The short X.Y version
