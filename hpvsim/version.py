766cadd9341fb5bab424304d907ecdee78e33cf3
Cliff Kerr <questionably@gmail.com>
2025-03-25 09:39:24 -0700

    update version and changelog

--- hpvsim/version.py
+++ hpvsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '2.0.1'
-__versiondate__ = '2024-02-14'
-__license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2023 by IDM'
+__version__ = '2.1.0'
+__versiondate__ = '2025-03-25'
+__license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2023-2025 by IDM'
